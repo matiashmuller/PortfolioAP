0d514f3779afed3ab786cfb04e4294b77ab32094
Matías <mullermatiash@gmail.com>
2022-07-26 16:30:18 -0300

    Iniciado backend con conexión a base

---
+++ Backend/src/main/java/com/portfolio/mhm/MhmApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mhm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhmApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/portfolio/mhm/MhmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mhm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

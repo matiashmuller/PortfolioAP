f3ba3b85401e4716c8570660fd827762fe810966
Matías <mullermatiash@gmail.com>
2022-07-27 22:14:56 -0300

    Comenzado login con jwt

---
+++ Backend/src/main/java/com/portfolio/mhm/Security/Repository/iRolRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.mhm.Security.Repository;
+
+import com.portfolio.mhm.Security.Entity.Rol;
+import com.portfolio.mhm.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Matt
+ */
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer>{
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}

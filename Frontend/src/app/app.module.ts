71aee491bae74b74913b98621016089d1a516391
Matías <mullermatiash@gmail.com>
2022-07-30 01:14:41 -0300

    Implementado login jwt también en front + botón inicio y cierre sesión

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -16,6 +16,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClient, HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -30,15 +32,18 @@ import { LoginComponent } from './components/login/login.component';
     ProyectoComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
